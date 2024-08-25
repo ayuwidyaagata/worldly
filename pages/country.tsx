3893664d8eb9e94f964747fb288de991d144f2e2
ayuwidyaagata <ayuwagata@gmail.com>
2024-08-25 15:22:43 +0700

    chore: create home and country page

---
+++ pages/country.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function CountryPage() {
+  return (
+    <>
+      <Head>
+        <title>Country List</title>
+      </Head>
+      <div>Country List</div>
+    </>
+  );
+}
---
+++ pages/home.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Worldly</title>
+      </Head>
+      <div>Home Page</div>
+    </>
+  );
+};
+
+export default Home;
