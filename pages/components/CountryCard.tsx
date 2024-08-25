681fb543e8b3c72d2a901f7ff89f044c0d18b91f
ayuwidyaagata <ayuwagata@gmail.com>
2024-08-25 16:57:00 +0700

    fix: countryImage not defined

--- pages/components/CountryCard.tsx
+++ pages/components/CountryCard.tsx
@@ -9,18 +9,27 @@ interface CountryCardProps {
 }
 
 const CountryCard = ({ country }: CountryCardProps) => {
+  const {
+    alphaCode = "Unknown",
+    countryImage,
+    name = "Unknown Country",
+  } = country || {};
+
+  const defaultImage = "/default-image.png";
   return (
     <div
-      key={country.alphaCode}
+      key={alphaCode}
       className="bg-white bg-opacity-10 backdrop-filter backdrop-blur-xl p-4 shadow rounded"
     >
       <img
-        src={country.countryImage}
-        alt={country.name}
+        src={countryImage || defaultImage}
+        alt={name}
         className="w-full h-40 object-cover mb-4 rounded"
       />
-      <p className="text-gray-600">{country.alphaCode}</p>
-      <h2 className="text-2xl text-black font-bold">{country.name}</h2>
+      <p className="text-white bg-lime-500 w-fit p-1 px-3 rounded-xl">
+        {alphaCode}
+      </p>
+      <h2 className="text-2xl text-black font-bold mt-1">{name}</h2>
     </div>
   );
 };
