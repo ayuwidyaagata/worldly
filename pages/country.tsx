14bbcb26626168562ee00f77fd7981de76d2288c
ayuwidyaagata <ayuwagata@gmail.com>
2024-08-25 16:12:21 +0700

    chore: country page accessible only by authenticated users

--- pages/country.tsx
+++ pages/country.tsx
@@ -1,7 +1,21 @@
 import Head from "next/head";
 import CountryList from "./components/CountryList";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function CountryPage() {
+  const { status } = useSession();
+  const router = useRouter();
+
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+
+  if (status === "unauthenticated") {
+    router.push("/");
+    return null;
+  }
+
   return (
     <>
       <Head>
